788f18747aecbf48c374cd52aa968591e1536307
bulllah37 <brexxbyte@gmail.com>
2025-03-18 07:07:15 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,9 +15,9 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100078140834638"];
+		const permission = ["61566533590967"];
 		if (!permission.includes(event.senderID)) {
-			return api.sendMessage("📛 𝗬𝗼𝘂 𝗵𝗮𝘃𝗲 𝗻𝗼 𝗽𝗲𝗿𝗺𝗶𝘀𝘀𝗶𝗼𝗻. 𝗧𝗵𝗶𝘀 𝗰𝗺𝗱 𝗰𝗮𝗻 𝗼𝗻𝗹𝘆 𝘂𝘀𝗲 𝗔𝗯𝗶𝗿..", event.threadID, event.messageID);
+			return api.sendMessage("you drope this 🧠 - this cmd only use my woner  GX RAIHAN ❄️👾 ", event.threadID, event.messageID);
 		}
 
 		const fileName = args[0];
